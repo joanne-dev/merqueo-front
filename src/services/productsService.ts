e2068330f24f0cb7638669e04be5aab51f02a25f
Leidy <leidyjoanne86@gmail.com>
2022-02-21 12:42:53 -0500

    LOG ENV

--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,5 +1,6 @@
 const ENDPOINT = 'http://localhost:9021';
 export const getProducts = async () => {
+    console.log('env: ', process.env.NODE_ENV);
     const url = `${ENDPOINT}/products`;
     const response = await fetch(url);
     const products = await response.json();
